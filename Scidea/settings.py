4e21bb757749223910f6fd1fb4d9e8498850fba5
Shaurya <shaurya.shauryagoyal123@gmail.com>
2024-11-22 23:46:42 +0530

    UI Changes 2(Static Files Handled - 2)

--- Scidea/settings.py
+++ Scidea/settings.py
@@ -126,10 +126,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# URL to serve static files
+STATIC_URL = '/static/'
 
+# Directory where static files will be collected for production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Additional directories to look for static files during development
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
