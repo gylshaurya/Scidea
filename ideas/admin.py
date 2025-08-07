63c21e5475402c5ba9e84e45a7ca4f0861657041
Shaurya <shaurya.shauryagoyal123@gmail.com>
2025-08-07 11:40:47 +0530

    Fixing Admin Panel

--- ideas/admin.py
+++ ideas/admin.py
@@ -1,12 +1,30 @@
 from django.contrib import admin
-from .models import Post, Tag
+from .models import Post, Tag, Upvote, Comment
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'status', 'created_at')
     list_filter = ('status', 'tags')
     search_fields = ('title', 'content')
+    date_hierarchy = 'created_at'
+    ordering = ('-created_at',)
+    filter_horizontal = ('tags',)
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Upvote)
+class UpvoteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('user__username', 'post__title')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('user__username', 'post__title', 'content')
+    date_hierarchy = 'created_at'
+    ordering = ('-created_at',)
